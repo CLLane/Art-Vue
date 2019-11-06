625edb5558dcba7ecba3069c08ae711cf30fa509
Chris Lane <pankokrumbs@gmail.com>
2019-11-05 18:22:38 -0700

    Refactor the next image button

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const apiKey = 'f7f4e420-ff32-11e9-b2a1-9941f14f4865';
+
+
+export default apiKey;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,5 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This website is designed for those that would like to browse through fine art.</h1>
   </div>
 </template>
